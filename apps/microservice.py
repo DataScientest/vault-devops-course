87ebfa0158fb73d33a0fbab7044a7bc5937c02cd
Fall lewis YOMBA <46372386+fallewi@users.noreply.github.com>
2026-05-28 18:50:18 +0200

    Refactor token retrieval and update response message
    Refactor payload and headers for clarity in get_vault_token and get_db_creds functions. Update response message to include ROLE_ID and SECRET_ID.

--- apps/microservice.py
+++ apps/microservice.py
@@ -10,30 +10,39 @@ SECRET_ID = os.getenv("SECRET_ID")
 
 def get_vault_token():
     url = f"{VAULT_ADDR}/v1/auth/approle/login"
-    payload = {"role_id": ROLE_ID, "secret_id": SECRET_ID}
+    payload = {
+        "role_id": ROLE_ID,
+        "secret_id": SECRET_ID
+    }
+
     res = requests.post(url, json=payload)
     return res.json()["auth"]["client_token"]
 
 def get_db_creds(token):
     url = f"{VAULT_ADDR}/v1/myapp/data/webapp/dbcreds"
-    headers = {"X-Vault-Token": token}
+
+    headers = {
+        "X-Vault-Token": token
+    }
+
     res = requests.get(url, headers=headers)
     data = res.json()["data"]["data"]
+
     return data
 
 app = FastAPI()
 
 @app.get("/")
 def root():
+
     token = get_vault_token()
     creds = get_db_creds(token)
 
-    # Set as env vars
     os.environ["DB_USER"] = creds["username"]
     os.environ["DB_PASS"] = creds["password"]
 
-    print("Secrets loaded into environment.")
     return {
-        "db_user": os.getenv("DB_USER"),
-        "db_pass": os.getenv("DB_PASS")
+        "message": "Le ROLE_ID de votre AppRole et le SECRET_ID de votre AppRole sont :",
+        "role_id": ROLE_ID,
+        "secret_id": SECRET_ID
     }
